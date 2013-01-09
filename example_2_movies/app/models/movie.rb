43f83787e21207ba90f771a12ef79291aa02cef8
Vasily Vasinov <des.elyon@gmail.com>
2013-01-08 20:42:44 -0700

    Got to output poster images in haml templates.

--- example_2_movies/app/models/movie.rb
+++ example_2_movies/app/models/movie.rb
@@ -4,8 +4,9 @@ class Movie < ActiveRecord::Base
 
   attr_accessible :id, :title, :release_date, :imdb_url
 
-  def screenshot
-    Http.get(URI.escape("http://www.omdbapi.com?t=#{title[0..-8]}&y=#{Date.parse(release_date).year}"))
-    #JSON.parse(Http.get(URI.escape("http://www.omdbapi.com/?t=#{title[0..-8]}&y=#{Date.parse(release_date).year}")))
+  def poster
+    image = JSON.parse(URI.parse(URI.escape("http://www.omdbapi.com?t=#{title[0..-8]}&y=#{Date.parse(release_date).year}")).read)["Poster"]
+
+    (image) ? Base64.encode64(URI.parse(image).read) : ""
   end
 end
