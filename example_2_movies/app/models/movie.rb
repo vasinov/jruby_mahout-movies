50a799842964925393b3eda539db275de4de6e6f
Vasily Vasinov <des.elyon@gmail.com>
2013-01-09 16:26:43 -0700

    Added similar_movies and plot methods to the movies model.

--- example_2_movies/app/models/movie.rb
+++ example_2_movies/app/models/movie.rb
@@ -4,9 +4,40 @@ class Movie < ActiveRecord::Base
 
   attr_accessible :id, :title, :release_date, :imdb_url
 
-  def poster
-    image = JSON.parse(URI.parse(URI.escape("http://www.omdbapi.com?t=#{title[0..-8]}&y=#{Date.parse(release_date).year}")).read)["Poster"]
+  def similar_movies
+    db_config = Rails.configuration.database_configuration[Rails.env]
+    recommender = JrubyMahout::Recommender.new(
+        "PearsonCorrelationSimilarity",
+        5,
+        "GenericItemBasedRecommender",
+        false)
+    recommender.data_model = JrubyMahout::DataModel.new("postgres", {
+        :host => db_config["host"],
+        :port => db_config["port"],
+        :db_name => db_config["database"],
+        :username => db_config["username"],
+        :password => db_config["password"],
+        :table_name => Preference.table_name
+    }).data_model
 
-    (image) ? Base64.encode64(URI.parse(image).read) : ""
+    Movie.find(recommender.similar_items(id, 5, nil))
+  end
+
+  def poster
+    begin
+      image = JSON.parse(URI.parse(URI.escape("http://www.omdbapi.com?t=#{title[0..-8]}&y=#{Date.parse(release_date).year}")).read)["Poster"]
+
+      (image) ? Base64.encode64(URI.parse(image).read) : ""
+    rescue Exception => e
+      ""
+    end
+  end
+
+  def plot
+    begin
+      JSON.parse(URI.parse(URI.escape("http://www.omdbapi.com?t=#{title[0..-8]}&y=#{Date.parse(release_date).year}&plot=full")).read)["Plot"]
+    rescue Exception => e
+      ""
+    end
   end
 end
