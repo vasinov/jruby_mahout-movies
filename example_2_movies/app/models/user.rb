ff0c164e7695ca110bdc5af323438a4bde9da191
Vasily Vasinov <des.elyon@gmail.com>
2013-01-10 17:30:37 -0700

    Added recommendations method to the user model

--- example_2_movies/app/models/user.rb
+++ example_2_movies/app/models/user.rb
@@ -7,4 +7,23 @@ class User < ActiveRecord::Base
 
   attr_accessible :id, :email, :password, :password_confirmation, :remember_me,
                   :age, :gender, :occupation, :zip
+
+  def recommendations
+    db_config = Rails.configuration.database_configuration[Rails.env]
+    recommender = JrubyMahout::Recommender.new(
+        "EuclideanDistanceSimilarity",
+        3,
+        "GenericUserBasedRecommender",
+        false)
+    recommender.data_model = JrubyMahout::DataModel.new("postgres", {
+        :host => db_config["host"],
+        :port => db_config["port"],
+        :db_name => db_config["database"],
+        :username => db_config["username"],
+        :password => db_config["password"],
+        :table_name => Preference.table_name
+    }).data_model
+
+    Movie.find(recommender.recommend(id, 10, nil))
+  end
 end
