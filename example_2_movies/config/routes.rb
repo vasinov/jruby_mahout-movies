fd3e00dd60ec1dfb6c65a2f9ef9881b7d09e227d
Vasily Vasinov <des.elyon@gmail.com>
2013-01-10 17:30:05 -0700

    Added users controller, views and routes

--- example_2_movies/config/routes.rb
+++ example_2_movies/config/routes.rb
@@ -1,7 +1,10 @@
 Movies::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :path => "", :path_names => { :sign_in => 'sign_in',
+                                                   :sign_out => 'sign_out'}
 
   root :to => "home#index"
 
   resources :movies
+
+  match 'profile' => 'users#profile', :via => :get
 end
