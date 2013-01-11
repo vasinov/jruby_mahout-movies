23e273289e95a3ee16e1dfcf07be60eca577dd54
Vasily Vasinov <des.elyon@gmail.com>
2013-01-10 17:30:18 -0700

    Changed the movie model

--- example_2_movies/app/models/movie.rb
+++ example_2_movies/app/models/movie.rb
@@ -2,13 +2,13 @@ class Movie < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :genres
 
-  attr_accessible :id, :title, :release_date, :imdb_url
+  attr_accessible :id, :title, :release_date, :rt_id, :rt_poster
 
   def similar_movies
     db_config = Rails.configuration.database_configuration[Rails.env]
     recommender = JrubyMahout::Recommender.new(
-        "PearsonCorrelationSimilarity",
-        5,
+        "EuclideanDistanceSimilarity",
+        3,
         "GenericItemBasedRecommender",
         false)
     recommender.data_model = JrubyMahout::DataModel.new("postgres", {
@@ -20,22 +20,12 @@ class Movie < ActiveRecord::Base
         :table_name => Preference.table_name
     }).data_model
 
-    Movie.find(recommender.similar_items(id, 5, nil))
-  end
-
-  def poster
-    begin
-      image = JSON.parse(URI.parse(URI.escape("http://www.omdbapi.com?t=#{title[0..-8]}&y=#{Date.parse(release_date).year}")).read)["Poster"]
-
-      (image) ? Base64.encode64(URI.parse(image).read) : ""
-    rescue Exception => e
-      ""
-    end
+    Movie.find(recommender.similar_items(id, 10, nil))
   end
 
   def plot
     begin
-      JSON.parse(URI.parse(URI.escape("http://www.omdbapi.com?t=#{title[0..-8]}&y=#{Date.parse(release_date).year}&plot=full")).read)["Plot"]
+      JSON.parse(URI.parse(URI.escape("http://www.omdbapi.com?t=#{title}&y=#{release_date}&plot=full")).read)["Plot"]
     rescue Exception => e
       ""
     end
