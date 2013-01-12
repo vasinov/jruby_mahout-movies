2ade6d7f271fa94c1295ce603021b3998750fc7f
Vasily Vasinov <des.elyon@gmail.com>
2013-01-11 17:34:08 -0700

    Added validation to the preference model.

--- example_2_movies/app/models/preference.rb
+++ example_2_movies/app/models/preference.rb
@@ -3,4 +3,8 @@ class Preference < ActiveRecord::Base
   belongs_to :item
 
   attr_accessible :user_id, :item_id, :rating, :created_at, :updated_at
+
+  validates_presence_of :user_id
+  validates_presence_of :item_id
+  validates :rating, :inclusion=> { :in => [1, 2, 3, 4, 5] }
 end
