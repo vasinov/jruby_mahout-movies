0ae9bbd859a6f8a020a8aaf2de8511932fff8c60
Vasily Vasinov <des.elyon@gmail.com>
2013-01-16 16:38:53 -0700

    Moved the moves example to the root of the repo.

--- example_2/example_2.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'bundler/setup'
-require 'jruby_mahout'
-
-recommender = JrubyMahout::Recommender.new("PearsonCorrelationSimilarity", 5, "GenericUserBasedRecommender", false)
-recommender.data_model = JrubyMahout::DataModel.new("file", { :file_path => "recommender_data.csv" }).data_model
-
-puts recommender.recommend(2, 10, nil)
